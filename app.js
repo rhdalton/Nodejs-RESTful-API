c9909047a33ad1f1d3eb2fe11e115d0b50b38376
Robin D <rhdalton@gmail.com>
2019-01-09 14:33:40 -0800

    Initial commit of nodejs app to github

---
+++ app.js
@@ -0,0 +1,91 @@
+/**
+ * Example RESTful API with GET, POST, PUT, DELETE actions
+ * Using Node.js Express web application framework
+ * This example app can interact with a MongoDB of movies
+ */
+const func = require('./functions');
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+// express middleware express.json()
+app.use(express.json());
+
+/** 
+ * GET: List of movie id, title, year
+ */
+app.get('/api/movies', (req, res) => {
+    res.header("Content-Type",'application/json');
+    res.send(JSON.stringify(func.getMovies(), null, 4));
+});
+/**
+ * GET: Movie details by Id
+ */
+app.get('/api/movies/:id', (req, res) => {
+    let movie = func.getMovieById(req.params.id);
+    // if movie id not found, return 404 error
+    if (!movie) return res.status(404).send('Movie not found');
+
+    // else show movie details
+    res.header("Content-Type",'application/json');
+    res.send(JSON.stringify(movie, null, 4));
+});
+
+/**
+ * POST: Create new movie
+ */
+app.post('/api/movies', (req, res) => {
+    // validate new movie
+    // get {result, error} with object destructuring
+    const { result, error } = func.validateMovie(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    // create new movie object and add to movies
+    const movie = {
+        id: movies.length + 1, // this would automatically be assigned if in real db
+        title: result.value.title,
+        year: result.value.year,
+        director: result.value.director,
+        cast: result.value.cast,
+        poster: result.value.poster
+    }
+    movies.push(movie);
+});
+
+/**
+ * PUT: Update movie by Id
+ */
+app.put('/api/movies/:id', (req, res) => {
+    // look up movie to check if it exists
+    let movie = func.getMovieById(req.params.id);
+
+    if (!movie) return res.status(404).send('Movie not found');
+
+    // validate movie, get {error} with object destructuring 
+    const { error } = func.validateMovie(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    // update movie values
+    movie.title = req.body.title;
+    movie.year = req.body.year;
+    movie.director = req.body.director;
+    movie.cast = req.body.cast;
+    movie.poster = req.body.poster;
+    res.send(movie);
+});
+
+/**
+ * DELETE: delete movie
+ */
+app.delete('/api/movies/:id', (req, res) => {
+    // check if movie exists
+    let movie = func.getMovieById(req.params.id);
+    if (!movie) return res.status(404).send('Movie not found');
+
+    // delete movie
+    const index = movies.indexOf(movie);
+    movies.splice(index, 1);
+    res.send(movies)
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}...`));
---
+++ functions.js
@@ -0,0 +1,61 @@
+
+// basic functions for this API
+const Joi = require('joi');
+
+// get movie by id from db
+function getMovieById(id) {
+    return movies.find(m => m.id === parseInt(id));
+}
+
+function validateMovie(movie) {
+    const schema = {
+        title: Joi.string().required(),
+        year: Joi.number().integer().min(1900).max(2100).required(),
+        director: Joi.string().required(),
+        cast: Joi.array().items(Joi.string()),
+        poster: Joi.string()
+    }
+    return Joi.validate(movie, schema);
+}
+
+function getMovies() {
+    return movies;
+}
+
+module.exports = {
+    getMovies,
+    getMovieById,
+    validateMovie
+}
+
+
+// example Json object return from MongoDB
+const movies = [
+    {
+        id: 1,
+        title: "The Dark Knight",
+        year: 2008,
+        genre: ["Action","Thriller"],
+        director: "Christopher Nolan",
+        cast: ["Christian Bale", "Aaron Eckhart", "Heath Ledger"],
+        poster: "top-gun.png"
+    },
+    {
+        id: 2,
+        title: "Titanic",
+        year: 1997,
+        genre: ["Drama", "Romance"],
+        director: "James Cameron",
+        cast: ["Leonardo DiCaprio", "Kate Winslet"],
+        poster: "titanic.png"
+    },
+    {
+        id: 3,
+        title: "The Avengers",
+        year: 2012,
+        genre: ["Fantasy", "Science Fiction"],
+        director: "Joss Whedon",
+        cast: ["Chris Evans", "Robert Downey Jr.", "Chris Hemsworth"],
+        poster: "the-avengers.png"
+    },
+]
