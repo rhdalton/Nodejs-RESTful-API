943aaa7f56936555a49cabc465d9aaaa9e973228
Robin D <rhdalton@gmail.com>
2019-01-13 17:55:48 -0800

    add return status for invalid objectid in get genre endpoint

--- routes/genres.js
+++ routes/genres.js
@@ -1,6 +1,7 @@
 const { Genre, validate } = require('../models/genre'); 
 const auth = require('../middleware/auth');
 const admin = require('../middleware/admin');
+const mongoose = require('mongoose');
 const express = require('express');
 const router = express.Router();
 
@@ -12,6 +13,9 @@ router.get('/', async (req, res) => {
 
 // Get Genre by Id, public access
 router.get('/:id', async (req, res) => {
+    if (!mongoose.Types.ObjectId.isValid(req.params.id))
+        return res.status(404).send('Invalid ID.');
+
     const genre = await Genre.findById(req.params.id);
 
     if (!genre) return res.status(404).send('Genre with given Id not found.');
